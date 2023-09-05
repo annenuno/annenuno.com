2299697176b27731b5d1dec6b2ffc426e9be7469
Ana <nunoany@gmail.com>
2023-09-04 22:17:37 -0500

    last commit of the day

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -8,11 +8,13 @@ export default function Home() {
   return (
     <main className="bg-[#FAFAFA]">
       <div className="flex flex-row">
-        <div className="bg-[#E3E5FF] border-[#8B8ECD] border-r-2 w-[120px] h-screen">
-          <div>heeeiiyrtcygbuhjinuihvftyuyhnijbtctrctygbijnibvtctree</div>
+        <div className="flex flex-col bg-[#E3E5FF] border-[#8B8ECD] border-r-2 w-[120px] h-screen">
+          <div>heeeiiyrtcygbuh</div>
+          <div>heecccceiiyruh</div>
         </div>
-        <div>
+        <div className="flex flex-col">
           <Menu />
+          <div className="bg-cyan-200">ndjebwibjn</div>
         </div>
       </div>
     </main>
