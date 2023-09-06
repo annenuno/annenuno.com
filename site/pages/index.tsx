74e14a5b97a4c835d77784aa038ca55233672059
Ana <nunoany@gmail.com>
2023-09-05 21:12:15 -0500

    sep 5

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -6,17 +6,17 @@ import Menu from "../components/menu";
 
 export default function Home() {
   return (
-    <main className="bg-[#FAFAFA]">
-      <div className="flex flex-row">
-        <div className="flex flex-col bg-[#E3E5FF] border-[#8B8ECD] border-r-2 w-[120px] h-screen">
-          <div>heeeiiyrtcygbuh</div>
+    <main className="flex flex-col bg-[#FAFAFA]">
+      <div className="flex flex-row h-[150px]">
+        <div className="flex flex-col bg-[#E3E5FF] border-[#8B8ECD] border-r-2 w-[120px]">
+          <div>heeeiiyrtcdcdcygbuh</div>
           <div>heecccceiiyruh</div>
         </div>
         <div className="flex flex-col">
           <Menu />
-          <div className="bg-cyan-200">ndjebwibjn</div>
         </div>
       </div>
+      <div className="bg-cyan-200 p-10">hello</div>
     </main>
   );
 }
