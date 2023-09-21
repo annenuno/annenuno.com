2d320301059b178c40afdc2144153e0210f6f167
Ana <nunoany@gmail.com>
2023-09-20 22:22:55 -0500

    header + svg

--- site/components/sections/footer.tsx
+++ site/components/sections/footer.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <>
-      <div className="flex flex-row">
-        <div className="border-[#CAD959]">This is the footer</div>
-        <div>3 dots</div>
+      <div className="flex flex-row text-[#366595]">
+        <div>This is the footer</div>
+        <div>
+          <Image
+            priority
+            src="/email.svg"
+            height={32}
+            width={32}
+            alt="Follow us on Twitter"
+          />
+        </div>
         <div>Top ^</div>
       </div>
     </>
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -4,13 +4,47 @@
 import Menu from "../components/sections/menu";
 import Main from "../components/pages/main";
 import Footer from "../components/sections/footer";
+import Image from "next/image";
 // import { Banner, Menu } from "../components/*.tsx";
 
 export default function Home() {
   return (
     <main className="flex flex-col bg-[#FAFAFA] h-auto">
       <header className="fixed top-0 left-0 z-[1000] w-[75px] h-screen p-0 border-r-2 border-[#8B8ECD] mb-0 bg-[#E3E5FF]">
-        <div className="overflow-hidden w-full flex items-center justify-center px-4"></div>
+        <div className="overflow-hidden w-full flex items-center justify-center px-4">
+          <div className="flex flex-col justify-around">
+            <div>
+              <Image
+                priority
+                src="/moon.svg"
+                height={32}
+                width={32}
+                alt="moon"
+                className="pt-10"
+              />
+            </div>
+            <div>
+              <Image
+                priority
+                src="/cat.svg"
+                height={32}
+                width={32}
+                alt="cat"
+                className="py-10"
+              />
+            </div>
+            <div>
+              <Image
+                priority
+                src="/email.svg"
+                height={32}
+                width={32}
+                alt="email"
+                className="pb-10"
+              />
+            </div>
+          </div>
+        </div>
       </header>
       <div className="h-screen">
         <div className="flex flex-row">
