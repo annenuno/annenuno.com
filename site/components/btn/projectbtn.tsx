aedb38c729734d319e8ff5b9573a15e2130780c6
Ana <nunoany@gmail.com>
2023-09-24 20:12:34 -0500

    shadow

--- site/components/btn/projectbtn.tsx
+++ site/components/btn/projectbtn.tsx
@@ -4,7 +4,7 @@ export default function Projectbtn() {
   return (
     <>
       <Link href="/annenuno">
-        <div className="z-10 flex flex-row gap-5 px-6 py-3 border-2 border-[#366595] rounded-full hover:bg-[#E7F3FF]">
+        <div className="z-10 flex flex-row gap-5 px-6 py-3 border-2 border-[#366595] rounded-full hover:bg-[#E7F3FF] shadow-md shadow-[#366595]">
           <p>2023</p>
           <p className="font-bold">annenuno.com</p>
           <p className="absolute right-[230px] px-2 text-[15px] border-2 border-[#366595] rounded-full">
